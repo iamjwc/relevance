ee8977602b0cf7eae7ae02f45d120cf2e4df7c84
Justin Camerer <iamjwc@gmail.com>
2010-05-20 10:00:24 -0400

    Looks like short circuiting works

--- neweval.rb
+++ neweval.rb
@@ -26,22 +26,51 @@ require 'rubygems'; require 'treetop'; require 'lib/relevance_grammar'; require 
 # 
 #    
 
-# class Relevance
-#   def initialize
-#     @tree = [:d, "&&", [:e, "||", [:f, "&&", :g]]]
-#   end
-# 
-#   def evaluate(env)
-#     stack = []
-# 
-#     def e(tree, stack)
-#       if tree.size == 1
-#         return node_value(env)
-#       else
-#       end
-#     end
-#   end
-# end
+class Rel
+  def initialize(tree)
+    @tree = tree
+  end
+
+  def op_predicate(op)
+    case op
+      when '==' then lambda {|a,b| a.call() == b.call()}
+      when '!=' then lambda {|a,b| a.call() != b.call()}
+      when '>=' then lambda {|a,b| a.call() >= b.call()}
+      when '<=' then lambda {|a,b| a.call() <= b.call()}
+      when '>'  then lambda {|a,b| a.call() >  b.call()}
+      when '<'  then lambda {|a,b| a.call() <  b.call()}
+      when '&&' then lambda {|a,b| a.call() && b.call()}
+      when '||' then lambda {|a,b| a.call() || b.call()}
+    end
+  end
+
+  def evaluate(env)
+    def e(tree, env)
+      p tree
+      a,op,b,*rest = tree
+
+
+      if op.nil?
+        # Only one item in the stack.
+        if a.is_a? Array
+          # 'a' is an expression
+          e(a, env)
+        elsif a.is_a? Symbol
+          # 'a' is a variable name
+          env[a]
+        else
+          # 'a' is a value
+          a
+        end
+      else
+        o = op_predicate(op)
+        rest.unshift o.call( lambda { e(a, env) }, lambda { e(b, env) })
+      end
+    end
+
+    e(@tree, env)
+  end
+end
 
 input = <<-INPUT.split("\n")
 a
@@ -65,8 +94,12 @@ INPUT
 input.each do |s|
   st = RelevanceParser.new.parse(s)
   puts s
-  p st.tree if st.respond_to? :tree
-  puts
+  if st.respond_to? :tree
+    t = st.tree
+    p t
+    p Rel.new(t).evaluate({:a => true, :b => false, :c => true, :d => true, :e => true, :f => true, :g => true})
+    puts
+  end
 end
 
 input.each do |s|
